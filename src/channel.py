3d6e1414e6ad0e009e0a49e77384a182747c155c
Victoria Kashpur <vikki0011@gmail.com>
2023-06-13 12:05:50 +1000

    done hw2

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,5 @@
 import os
+import json
 from googleapiclient.discovery import build
 
 
@@ -10,8 +11,35 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
+        self.url = f"https://www.youtube.com/channel/{channel_id}"
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.subscribers_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
+        self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])
 
     def print_info(self):
         """Выводит в консоль информацию о канале."""
-        self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute())
+        data = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(data)
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API"""
+        return cls.youtube
+
+    def to_json(self, filename):
+        """сохраняет в файл значения атрибутов экземпляра `Channel`"""
+        data = {
+            "channel_id": self.channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscribers_count": self.subscribers_count,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
+
+        with open(filename, 'w', encoding='UTF-8') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
