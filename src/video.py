01696e27258192683f261c6c0bb1465c84996db1
Victoria Kashpur <vikki0011@gmail.com>
2023-07-01 20:44:40 +1000

    done hw6

--- src/video.py
+++ src/video.py
@@ -8,13 +8,21 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id):
-        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+        try:
+            video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                              id=video_id).execute()
-        self.video_id = video_id
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.video_url = f"https://youtu.be/{video_response['items'][0]['id']}"
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+            self.video_id = video_id
+            self.video_title = video_response['items'][0]['snippet']['title']
+            self.video_url = f"https://youtu.be/{video_response['items'][0]['id']}"
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+
+        except:
+            self.id = video_id
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.video_title
